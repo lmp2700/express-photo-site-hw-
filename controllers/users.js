77d6a316f1176fd577f1672084926fe025b4baea
Liz Steak <lizmaepark@gmail.com>
2018-10-17 12:29:20 -0600

    updated with async await code

--- controllers/users.js
+++ controllers/users.js
@@ -5,25 +5,47 @@ const Photos = require('../models/photomodel');
 
 // just get create, edit and delete for the HW
 
-router.get('/', (req, res) => {
-    User.find({}, (err, foundUsers) => {
-        res.render('users/index.ejs', {
-            users: foundUsers
-        });
-    })
+router.get('/', async (req, res) => {
+  try {
+    const foundUsers = await User.find();
+    res.render('users/index.ejs', {
+      users: foundUsers
+    });
+  } catch(err) {
+    res.send(err)
+    }
 });
 
+router.get('/new', async (req, res) => {
+  try {
+    const newUsers = await User.find();
+    res.render('users/new.ejs', {
+      user: newUsers
+    });
+    } catch(err) {
+      res.send(err)
+    }
+  });
 
-router.get('/new', (req, res) => {
-    res.render('users/new.ejs');
-  })
+router.post('/', async (req, res) => {
+    try {
+      const createUser = await User.create(req.body);
+      res.redirect('/users')
+    } catch(err) {
+      res.send(err)
+    }
+  });
+  
 
-router.get('/users', (req, res) => {
-  User.find({}, (err, allUsers) => {
+router.get('/users', async (req, res) => {
+  try {
+    const allUsers = await User.find();
     res.render('users/users.ejs', {
       user: allUsers
-    });
-  });
+    })
+  } catch(err) {
+    res.send(err)
+  }
 });
 
 // There should be a show page for the user's photos. On this page, you should be able to see the user's username, 
@@ -32,39 +54,40 @@ router.get('/users', (req, res) => {
 // 2-Model - Think about it: Is the user’s id a part of the route? In which case there would be two params for 
 // one user’s particular photo. Would that look like users/1/8 or like photos/1/8?
 
-router.get('/:id', (req, res)=>{
-  User.findById(req.params.id, (err, foundUser) => {
-    // push the photos into the array for the user
+router.get('/:id', async (req, res)=>{
+  try {
+    const findUser = await User.findById(req.params.id);
+    // push the photos into an array?
     res.render('users/show.ejs', {
-      user: foundUser,
-      photos: Photos
+      user: findUser
     });
-  });
+  } catch(err) {
+    res.send(err)
+  }
 });
 
 // If you click on button to edit a user, it should take you to an edit form.
 // If you click on the delete button, the user should be deleted.
 // 2-Model - Think about it: If we delete a user, we also need to delete their photos. 
 // Will that automatically happen when we delete the user?
-router.get('/:id/edit', (req, res) => {
-  User.findByIdAndUpdate(req.params.id, (err, editUser) => {
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const editUser = await User.findByIdAndUpdate(req.params.id);
     res.render('users/edit.ejs', {
       user: editUser
     });
-  });
+  } catch(err) {
+    res.send(err)
+  }
 });
+  // User.findByIdAndUpdate(req.params.id, (err, editUser) => {
+  //   res.render('users/edit.ejs', {
+  //     user: editUser
+  //   });
+  // });
 
 
-router.post('/', (req, res) => {
-  User.create(req.body, (err, createdUser) => {
-    if(err) {
-      console.log(err)
-    } else {
-      console.log(createdUser)
-      res.redirect('/users')
-      }
-  });
-});
+
 
 // CODE FOR PUSHING PHOTOS TO THE PAGE??
 // router.post("/:id", (req, res) => {
@@ -78,27 +101,28 @@ router.post('/', (req, res) => {
 //   });
 // });
 
-router.put('/:id', (req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body, (err, updateUser) => {
-    res.redirect('/users');
-  })
-})
-
-router.delete('/:id', (req, res) => {
-  User.findOneAndDelete(req.params.id, (err, deleteUser) => {
-    res.render('users/show.ejs')
-  });
+router.put('/:id', async (req, res) => {
+  try {
+    const updateUser = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect('/users')
+  } catch(err) {
+    res.send(err)
+  }
 });
+  // User.findByIdAndUpdate(req.params.id, req.body, (err, updateUser) => {
+  //   res.redirect('/users');
+  // })
 
-
-
-
-
-
-
-
-
-
-
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteUser = await User.findOneAndDelete(req.params.id);
+    res.render('/users/show.ejs')
+  } catch(err) {
+    res.send(err)
+  }
+});
+  // User.findOneAndDelete(req.params.id, (err, deleteUser) => {
+  //   res.render('users/show.ejs')
+  // });
 
 module.exports = router;
